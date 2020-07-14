a0b5822243620749ff982205c7de6c1a1d0d0a8c
Abdur Rahim <abdoerrahiem@gmail.com>
2020-07-14 14:15:44 +0700

    edited server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ MongoDB()
 app.use(express.json())
 app.use(cors())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+
 app.get('/', (req, res) => res.send('API running'))
 
 app.use('/providers', require('./routes/providers'))
